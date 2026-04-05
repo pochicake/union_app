72f70cc3827c102eaec7c3126fe66c2f8f3011a3
pochi <gddev2001@gmail.com>
2026-04-05 11:20:39 +0800

    feat(ui): add pages

---
+++ lib/ui/pages/notes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Notes extends StatelessWidget {
+  const Notes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Notes"),
+      ],
+    );
+  }
+}
